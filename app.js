4cfb85f35ac94a1d24f5ef51c56c2170c50eb5af
Orion <orionvtg@gmail.com>
2020-10-24 18:19:06 -0700

    setting up the timer

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello")
+var timerCount = 100
+
+var timerEl = document.querySelector("#timer")
+
+timerEl.textContent = timerCount
+
+var timer = window.setInterval(function (){
+timerCount--;
+timerEl.textContent = timerCount
+
+if(timerCount === 0){
+  clearInterval(timer)
+}
+},1000)

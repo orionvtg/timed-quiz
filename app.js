4da6ae36656a68a36b3f607cc532a4661ca8d670
Orion <orionvtg@gmail.com>
2020-10-24 19:44:41 -0700

    adding function to stop the game

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var questions = [
   { 
     q: "What is your name?", 
     choices: ["Orion", "Hunter","Yamit"], 
-   a: "C" 
+   a: "A" 
   }, 
   { 
     q: "Where is home?", 
@@ -34,6 +34,12 @@ var questions = [
 
 
   function setQuestions() {
+
+    if (questionsPointer === questions.length) {
+      clearInterval(timer)
+      alert("You are done with " + timerCount + " seconds left!")
+      return
+    }
     question.textContent = questions[questionsPointer].q
     optionA.textContent = questions[questionsPointer].choices[0]
     optionB.textContent = questions[questionsPointer].choices[1]
@@ -45,24 +51,30 @@ var questions = [
   optionA.addEventListener("click", function () {
     if(optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
       console.log("right!")
+      questionsPointer++
+      setQuestions()
     } else {
-      console.log("wrong")
+      timerCount -= 10
     }
   })
 
   optionB.addEventListener("click", function () {
     if(optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
       console.log("right!")
+      questionsPointer++
+      setQuestions()
     } else {
-      console.log("wrong")
+      timerCount -= 10
     }
   })
 
   optionC.addEventListener("click", function () {
     if(optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
       console.log("right!")
+      questionsPointer++
+      setQuestions()
     } else {
-      console.log("wrong")
+      timerCount -= 10
     }
   })
   
